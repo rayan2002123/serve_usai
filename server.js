15c61dd531e14ea7d4ac9ec3ac2dff7d961650f0
rayan2002123 <rngasseu123@gmail.com>
2026-05-29 00:37:08 +0200

    fix cors

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const app = express()
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://united-students.vercel.app"
+    "https://united-students.vercel.app",
+    "https://eaved-suzi-shaftless.ngrok-free.dev"
   ],
   credentials: true
 }))
