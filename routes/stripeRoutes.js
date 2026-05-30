673f2d6291fb5e5169aef25dff7c15c54391bcf8
rayan2002123 <rngasseu123@gmail.com>
2026-05-30 22:29:56 +0200

    fix payemnt method

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -51,7 +51,7 @@ router.post(
 
       const session =
         await stripe.checkout.sessions.create({
-          payment_method_types: ['card'],
+          //payment_method_types: ['card'],
 
           mode: 'payment',
 
